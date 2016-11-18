25869ea43cba62b0e75b81e6cb0a5c471d3a10ce
theovast <theophile.vast@gmail.com>
2016-11-18 09:29:00 +0100

    add delte feature to profile

---
+++ dashboard/app/form.component.js
@@ -0,0 +1,44 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var bear_1 = require('./bear');
+var bear_service_1 = require('./bear.service');
+var router_1 = require('@angular/router');
+var FormComponent = (function () {
+    function FormComponent(router, bearService) {
+        this.router = router;
+        this.bearService = bearService;
+        this.bears = [];
+        this.active = true;
+        this.bear = new bear_1.Bear();
+        this.submitted = false;
+    }
+    FormComponent.prototype.onSubmit = function () { this.submitted = true; };
+    FormComponent.prototype.ngOnInit = function () {
+    };
+    FormComponent.prototype.saveBear = function (bear) {
+        var commentOperation;
+        this.bearService.addBear(this.bear).subscribe();
+        var link = ['/bear'];
+        this.router.navigate(link);
+    };
+    FormComponent = __decorate([
+        core_1.Component({
+            // moduleId: module.id,
+            selector: 'bear-form',
+            templateUrl: 'app/form.component.html'
+        }), 
+        __metadata('design:paramtypes', [router_1.Router, bear_service_1.BearService])
+    ], FormComponent);
+    return FormComponent;
+}());
+exports.FormComponent = FormComponent;
+//# sourceMappingURL=form.component.js.map
--- service/app/models/bear.js
+++ service/app/models/bear.js
@@ -1,15 +1,18 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
+	var mongoose     = require('mongoose');
+	var Schema       = mongoose.Schema;
 
-var BearSchema = new Schema({
-  name:    String,
-	photo:    String,
-	description:    String,
-	amis: [],
-	message:[],
-  age:     { type: Number, min: 18, max: 65 },
-	updated: { type: Date, default: Date.now },
+	var BearSchema = new Schema({
+		name : String,
+		cover : String,
+		profilePic :String,
+		description: String,
+		sounds : [],
+		infos : String,
+		gigs : String,
+		amis: [],
+		message:[],
+		age:     { type: Number, min: 18, max: 65 },
+		updated: { type: Date, default: Date.now },
+	});
 
-});
-
-module.exports = mongoose.model('Bear', BearSchema);
+	module.exports = mongoose.model('Bear', BearSchema);
