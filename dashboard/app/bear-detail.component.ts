a127a867ed05e6d6bbf40fa90f9fcf0851a6c89f
theovast <theophile.vast@gmail.com>
2016-11-20 15:40:59 +0100

    add a login form

--- dashboard/app/bear-detail.component.ts
+++ dashboard/app/bear-detail.component.ts
@@ -22,11 +22,12 @@ export class BearDetailComponent implements OnInit {
   private sub:any;
   private bear: Bear;
   private router: Router;
+  $:any;
 
   constructor(
     private bearService: BearService,
     private route: ActivatedRoute,
-    private location: Location,
+    private location: Location
     ) {}
 
   private static _emitters: { [ID: string]: EventEmitter<any> } = {};
@@ -73,6 +74,29 @@ export class BearDetailComponent implements OnInit {
 
     goBack(): void {
       this.location.back();
+    }
+
+    foo () {
+        this.$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    this.$('.modal').modal();
+
+      this.$('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+      ready: function(modal : any, trigger : any) { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+        console.log(modal, trigger);
+      },
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+  });
+      
     }
 
 
